0d24f2b979c1397d99e51db3257b0ab82717a3ef
Hui Su <yudasuhui@hotmail.com>
2018-09-12 14:33:06 +0200

    auto complete

--- www/js/api.js
+++ www/js/api.js
@@ -1,26 +1,36 @@
 var searchText;
 var jsonList;
+var ingredient = $('#ingredient');
 
-$.getJSON( "/json/food.json", (data)=> {
- jsonList=data;
-   
+$.getJSON("/json/food.json", (data) => {
+  jsonList = data;
+  //console.log(jsonList[0]);
+  ingredient.keyup(() => {
+    var inputText = ingredient[0].value;
+    console.log(inputText);
+  
+    search(jsonList, inputText);
     
-    console.log(jsonList[0]);
-    searchText="ägg";
-    search(jsonList, searchText);
-
   });
 
-  function search(jsonList, searchText){
-      let regEx = new RegExp(`(^|\\s)${searchText}(\\s|$)`, 'ig');
-      let result=jsonList.filter(x => x.Namn.match(regEx) !== null);
+});
+
+function search(jsonList, searchText) {
+  let regEx = new RegExp(`(^|\\s)${searchText}(\\s|$)`, 'ig');
+  //let regEx = new RegExp(searchText.split("").join("\\w*").replace(/\W/, ""), "i");
+  let result = jsonList.filter(x => x.Namn.match(regEx) !== null);
   console.log(result);
-    }
+}
 
-  // var input=$('#ingredient').val;
-  // console.log(input.value);
-  var ingredient= $('#ingredient');
-  ingredient.keyup(()=>console.log(ingredient[0].value)
- )
+function setSearch(value) {
+  document.getElementById('search').value = value;
+  document.getElementById("result").innerHTML = "";
+}
 
-
+function changeInput(val) {
+  var autoCompleteResult = matchPeople(val);
+  document.getElementById("result").innerHTML = "";
+  for (var i = 0, limit = 10, len = autoCompleteResult.length; i < len  && i < limit; i++) {
+    document.getElementById("result").innerHTML += "<a class='list-group-item list-group-item-action' href='#' onclick='setSearch(\"" + autoCompleteResult[i] + "\")'>" + autoCompleteResult[i] + "</a>";
+  }
+}
