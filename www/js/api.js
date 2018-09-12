7ce2dae7202956126b77aa9f203b8e97020527aa
Hui Su <yudasuhui@hotmail.com>
2018-09-12 15:32:20 +0200

    push

--- www/js/api.js
+++ www/js/api.js
@@ -1,16 +1,19 @@
 var searchText;
-var jsonList;
+var jsonList = [];
 var ingredient = $('#ingredient');
 
 $.getJSON("/json/food.json", (data) => {
+
+
   jsonList = data;
-  //console.log(jsonList[0]);
+
   ingredient.keyup(() => {
     var inputText = ingredient[0].value;
     console.log(inputText);
-  
-    search(jsonList, inputText);
-    
+    // changeInput(jsonList, inputText)
+    var list = search(jsonList, inputText);
+    changeInput(list)
+
   });
 
 });
@@ -19,18 +22,26 @@ function search(jsonList, searchText) {
   let regEx = new RegExp(`(^|\\s)${searchText}(\\s|$)`, 'ig');
   //let regEx = new RegExp(searchText.split("").join("\\w*").replace(/\W/, ""), "i");
   let result = jsonList.filter(x => x.Namn.match(regEx) !== null);
-  console.log(result);
+  return result
+
 }
 
-function setSearch(value) {
-  document.getElementById('search').value = value;
+function setSearch(val) {
+  document.getElementById('search').value = val;
   document.getElementById("result").innerHTML = "";
 }
 
-function changeInput(val) {
-  var autoCompleteResult = matchPeople(val);
+function changeInput(list) {
+  var autoCompleteResult = list;
+  console.log(autoCompleteResult);
   document.getElementById("result").innerHTML = "";
-  for (var i = 0, limit = 10, len = autoCompleteResult.length; i < len  && i < limit; i++) {
-    document.getElementById("result").innerHTML += "<a class='list-group-item list-group-item-action' href='#' onclick='setSearch(\"" + autoCompleteResult[i] + "\")'>" + autoCompleteResult[i] + "</a>";
+  for (var i = 0, len = autoCompleteResult.length; i < len; i++) {
+   var listText=autoCompleteResult[i].Namn;
+   //console.log(listText)
+   //var onclickFn=setSearch(listText);
+   document.getElementById("result").innerHTML += "<a class='list-group-item list-group-item-action' href='#' onclick='setSearch("+ listText +")' >" + autoCompleteResult[i].Namn + "</a>";
+  
   }
+  
 }
+
