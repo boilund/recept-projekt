9cd0c2f047588cfba94f4e65974b3fa36b53e777
Hui Su <yudasuhui@hotmail.com>
2018-09-17 11:02:21 +0200

    add features
    time to new recept,
    change port. select to from 1 to 10,
    finish auto complete function

--- www/js/api.js
+++ www/js/api.js
@@ -4,13 +4,11 @@ var ingredient = $('#ingredient');
 
 $.getJSON("/json/food.json", (data) => {
 
-
   jsonList = data;
 
   ingredient.keyup(() => {
     var inputText = ingredient[0].value;
-    console.log(inputText);
-    // changeInput(jsonList, inputText)
+    //console.log(inputText);
     var list = search(jsonList, inputText);
     changeInput(list)
 
@@ -19,29 +17,40 @@ $.getJSON("/json/food.json", (data) => {
 });
 
 function search(jsonList, searchText) {
-  if(searchText){
-  let regEx = new RegExp(`(^|\\s)${searchText}(\\s|$)`, 'ig');
-  //let regEx = new RegExp(searchText.split("").join("\\w*").replace(/\W/, ""), "i");
-  let result = jsonList.filter(x => x.Namn.match(regEx) !== null);
-  return result;
+  if (searchText) {
+    if(searchText.toLowerCase()!=="vatten"){
+      let regEx = new RegExp(`(^|\\s)${searchText}(\\s|$)`, 'ig');
+    // let regEx = new RegExp(searchText.split("").join("\\w*").replace(/\W/, ""),
+    // "i");
+    let result = jsonList.filter(x => x.Namn.match(regEx) !== null);
+    return result;
+    }else{
+      setSearch("vatten");
+    }
+   
   }
 
 }
 
-function setSearch(val) {
-  document.getElementById('search').value = val;
-  document.getElementById("result").innerHTML = "";
+function setSearch(val) { 
+  $('input[name=ingredient]').val(val);
+  document
+    .getElementById("result")
+    .innerHTML = "";
 }
 
 function changeInput(list) {
-  var autoCompleteResult = list;
-  console.log(autoCompleteResult);
-  document.getElementById("result").innerHTML = "";
-  for (var i = 0, len = autoCompleteResult.length; i < len; i++) {
-   var listText=autoCompleteResult[i].Namn;
-   console.log(i,listText)
-   document.getElementById("result").innerHTML += '<a class="list-group-item list-group-item-action" href="#"" onclick="setSearch('+ listText +')" >  ' + listText + '   </a>';
+  if(list){
+    var autoCompleteResult = list;
+    document
+      .getElementById("result")
+      .innerHTML = "";
+    for (var i = 0, len = autoCompleteResult.length; i < len; i++) {
+      var listText = autoCompleteResult[i].Namn;
+      $("#result").append(`<a class='list-group-item list-group-item-action' onclick='setSearch("${listText}")' >  ${listText}   </a>`);
+    }
   }
   
+    
 }
 
