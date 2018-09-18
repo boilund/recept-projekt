ed8c14ffa4c0ec2e0ec99ae390cb7f41b3d9c139
Nana Mochihara <nana.mochihara@gmail.com>
2018-09-18 15:17:35 +0200

    Create template for recipe card in "Mina sidor"

--- www/js/classes/recipe.class.js
+++ www/js/classes/recipe.class.js
@@ -1,5 +1,13 @@
 class Recipe extends Base {
   constructor() {
     super();
+
+    // test
+    this.url = 'apple_pie';
+    this.img = 'apple-pie.jpg';
+    this.title = 'Äpplepaj';
+    this.author = 'catarina';
+    this.time = '40 min';
+    this.likes = 79;
   }
 }
