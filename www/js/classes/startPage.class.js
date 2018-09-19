45e19063e27332a6c3117d5c6efb9dc16ebab2a4
Unknown <minmilera@gmail.com>
2018-09-19 21:55:58 +0200

    Add click function and auto complete function (in progress)

--- www/js/classes/startPage.class.js
+++ www/js/classes/startPage.class.js
@@ -2,6 +2,59 @@ class StartPage extends Base {
   constructor(app) {
     super();
     this.app = app;
+     this.load();
+    // this.autoComplete();
+    // this.keyup();
+
+
   }
+
+
+  // search field
+  click(e) {
+    if ($(e.target).hasClass('search-input')) {
+      console.log('AAAAAAAAAa');
+      // console.log(this.app.recipes.title);
+
+    }
+  }
+
+  // keyup(e) {
+  //   console.log('keyup, input!', e);
+  // }
+
+
+  // autocomplete
+  async load() {
+    $.getJSON('/json/recipe.json').then((data) => {
+      this.autoComplete(data);
+    });
+  }
+
+
+
+  autoComplete(data) {
+    console.log('this', this);
+    let that = this;
+    console.log('that', that);
+
+     $(document).on('keyup', '.search-input', function(e) {
+      console.log('e', e);
+      let inputText = $(e.target).val();
+      console.log('inputText:', inputText);
+
+      if (inputText) {
+        var list = that.search(data, inputText);
+        console.log(list)
+        that.changeInput(e, list);
+      } else {
+        $(e.target).next().empty();
+      }
+
+
+    })
+
+  }
+
 
 }
