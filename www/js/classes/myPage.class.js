5c9f9fdd1eac082cf177f3534bf2f1fd31aeef8d
Monika Lukic <monika.lukik@hotmail.com>
2018-09-19 13:59:11 +0200

    make show more faster

--- www/js/classes/myPage.class.js
+++ www/js/classes/myPage.class.js
@@ -6,15 +6,10 @@ class MyPage extends Base {
   }
 
   pickCards() {
-    const {
-      user
-    } = this.app;
+    const { user } = this.app;
 
     this.myFavorites = this.app.recipes.filter(recipe => recipe.favorite);
     this.myRecipes = this.app.recipes.filter(recipe => user === recipe.author);
-
-    console.log('recipe', this.app.recipes);
-    console.log('fa', )
   }
 
 }
--- www/js/classes/recipe.class.js
+++ www/js/classes/recipe.class.js
@@ -13,6 +13,11 @@ class Recipe extends Base {
     $(e.target).hasClass('comments-btn') && $('.comments').toggle();
   }
 
+  showMore() {
+    $('.recipeCard:hidden').slice(0, this.app.startPage.sliceNr).show(10);
+    $('.recipeCard:hidden').length == 0 && $('.more-btn').hide();
+  }
+
   click3(e) {
     if ($(e.target).hasClass('fa-heart')) {
       this.varyLikes();
@@ -20,6 +25,7 @@ class Recipe extends Base {
       this.app.myPage.pickCards();
       this.app.popState.startPage();
       this.render('', 3);
+      this.showMore();
     }
   }
 
