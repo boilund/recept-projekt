a6f1d44f66c91b858875cf1792b54871027d2085
Nana Mochihara <nana.mochihara@gmail.com>
2018-09-19 16:39:12 +0200

    Show more cards when clicking 'visa mer' button in 'mina sidor'

--- www/js/classes/myPage.class.js
+++ www/js/classes/myPage.class.js
@@ -3,6 +3,7 @@ class MyPage extends Base {
     super();
     this.app = app;
     this.pickCards();
+    this.sliceNr = 8;
   }
 
   pickCards() {
@@ -10,6 +11,14 @@ class MyPage extends Base {
 
     this.myFavorites = this.app.recipes.filter(recipe => recipe.favorite);
     this.myRecipes = this.app.recipes.filter(recipe => user === recipe.author);
+  }
+
+  click(e) {
+    if ($(e.target).hasClass('more-btn')) {
+      $('.recipeCard:hidden').slice(0, 8).show(10);
+      $('.recipeCard:hidden').length == 0 && $(e.target).hide();
+      this.slices + 8;
+    }
   }
 
 }
