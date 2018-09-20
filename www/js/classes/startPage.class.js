106f6e5f93ed01d59e8f1ebe4b285f1ac20aadd9
Nana Mochihara <nana.mochihara@gmail.com>
2018-09-20 08:23:57 +0200

    Implement pickFilteredCards function

--- www/js/classes/startPage.class.js
+++ www/js/classes/startPage.class.js
@@ -7,22 +7,33 @@ class StartPage extends Base {
   }
 
   click2(e) {
-   if($(e.target).hasClass('more-btn')) {
-     $('.recipeCard:hidden').slice(0, 8).show(10);
-     $('.recipeCard:hidden').length == 0 && $(e.target).hide();
-     this.slices + 8;
-   }
+    if ($(e.target).hasClass('more-btn')) {
+      $('.recipeCard:hidden').slice(0, 8).show(10);
+      $('.recipeCard:hidden').length == 0 && $(e.target).hide();
+      this.slices + 8;
+    }
+  }
+
+  pickFilteredCards(selectedCategory) {
+    this.filteredCards = [];
+
+    const isIncluded = (category) => {
+      return selectedCategory.some(item => category.includes(item));
+    };
+    this.filteredCards = this.app.recipes.filter(recipe => isIncluded(recipe.category));
+
+    this.app.popState.startPage();
+    this.app.recipe.render('', 3);
   }
 
   change2(e) {
-    this.selectedCategory = [];
-
     if ($(e.target).hasClass("form-check-input")) {
+      this.selectedCategory = [];
       const checkedElements = $("input:checked");
       for (let i = 0; i < checkedElements.length; i++) {
         this.selectedCategory.push(checkedElements[i].value);
       }
     }
-    console.log(this.selectedCategory);
+    this.pickFilteredCards(this.selectedCategory);
   }
 }
