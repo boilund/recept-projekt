141f22eb479600c0da1f065e675823be88f0cbbc
Hui Su <yudasuhui@hotmail.com>
2018-09-20 14:09:30 +0200

    branch!!

--- www/js/classes/createRecipe.class.js
+++ www/js/classes/createRecipe.class.js
@@ -9,16 +9,37 @@ class CreateRecipe extends Base {
     this._stepsList = [];
     this._ingredientsList = [];
     this._ingredientsList.push(new Ingredients(this));
+    this._categoriesList = [];
+    this._nutrients = {};
+
 
   }
 
   get recipeTitle() {
-    return `${this._recipeTitle}`;
+    return this._recipeTitle;
   }
 
   set recipeTitle(val) {
     this._recipeTitle = val;
   }
+
+  get portions() {
+    return this._portions;
+  }
+
+  set portions(val) {
+    this._portions = val;
+  }
+
+  get time() {
+    return this._time;
+  }
+
+  set time(val) {
+    this._time = val;
+  }
+
+
 
 
 
@@ -31,9 +52,17 @@ class CreateRecipe extends Base {
       let that = this;
 
       let ingredient = new Ingredients(this);
+      //that.app.ingredient.createIngredient();
       that._ingredientsList.push(ingredient);
       $(".add-ingr").empty();
       that._ingredientsList.render(".add-ingr", "");
+
+    }
+
+    if (target.hasClass("submint-btn")) {
+      this.calcPortionNutrition();
+      let json = this.createRecipe();
+      //this.saveRecipe(json);
 
     }
 
@@ -43,6 +72,19 @@ class CreateRecipe extends Base {
 
   change(event) {
     this.labelCss(event);
+    let target = $(event.target);
+    if ((target).hasClass("portions")) {
+      let that = this;
+      that._portions = target.val();
+    }
+
+    if ((target).hasClass("form-check-input")) {
+      let that = this;
+      let category = target.val();
+      that._categoriesList.push(category);
+    }
+    //console.log(this._categoriesList);
+
   }
 
   keyup(event) {
@@ -53,9 +95,78 @@ class CreateRecipe extends Base {
     if (target.hasClass("recept-name")) {
       this._recipeTitle = target.val();
     }
+    //get cooking time
+    if (target.hasClass("time")) {
+      this._time = target.val();
+    }
+
+
 
 
   }
+  createRecipe() {
+    let newRecipe = {};
+    newRecipe.favorite = true;
+    newRecipe.title = this._recipeTitle;
+    //newRecipe.img =this._img;
+    newRecipe.time = this._time;
+    newRecipe.likes = 1;
+    newRecipe.category = this._categoriesList;
+    newRecipe.author = "Catarina Bennetoft";
+    newRecipe.url = Date.now();
+    newRecipe.defaultPortion = this._portions;
+    newRecipe.ingredient = this._ingredientsList;
+    newRecipe.instructions = this._stepsList;
+    newRecipe.nutrition = this._nutrients;
+    newRecipe.comments = [];
+   // newRecipe.⚙ = "Recipe";
+
+
+    return newRecipe;
+  }
+
+  calcPortionNutrition() {
+    let that = this;
+    //console.log(this._ingredientsList);
+    let nList = [];
+    that._ingredientsList.forEach((i) => {
+      if (i.name) {
+        nList.push(i.itemNutrients);
+      }
+
+      // let i= Ingredients;
+      // console.log(i);
+    })
+    let kj = 0;
+    let kcal = 0;
+    let fat = 0;
+    let saturatedFat = 0;
+    let carbohydrates = 0;
+    let protein = 0;
+    let salt = 0;
+
+    for (let i = 0; i < nList.length; i++) {
+      let l = nList[i];
+      kj += l.EnergyKJ;
+      kcal += l.EnergyKCAL;
+      fat += l.Fat;
+      saturatedFat += l.TotalMonounsaturatedFattyAcids + l.TotalPolyunsaturatedFattyAcids;
+      carbohydrates += l.Carbohydrates;
+      protein += l.Protein;
+      salt += l.Salt;
+    }
+
+    that._nutrients.kj = kj;
+    that._nutrients.kcal = kcal;
+    that._nutrients.fat = fat;
+    that._nutrients.saturatedFat = saturatedFat;
+    that._nutrients.carbohydrates = carbohydrates;
+    that._nutrients.protein = protein;
+    that._nutrients.salt = salt;
+    //console.log(that._nutrients)
+  }
+
+
 
   deleteIngr(item) {
     let i = this._ingredientsList.indexOf(item);
@@ -125,6 +236,17 @@ class CreateRecipe extends Base {
 
 
 
+  }
+
+  saveRecipe(json) {
+    let recipeList = [];
+    JSON._save('testCreate', {recept:json});
+    // $.getJSON('/json/testCreate.json').then((data) => {
+    //   recipeList = data;
+    //   recipeList.push(json);
+    //   console.log(recipeList)
+    //   JSON._save('testCreate', json);
+    //})
   }
 
 
