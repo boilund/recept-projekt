9dd4ad993d8f0c6105626e6622427668ef18abb9
Hui Su <yudasuhui@hotmail.com>
2018-09-21 15:21:35 +0200

    drag and drop file, but not showing review

--- www/js/classes/createRecipe.class.js
+++ www/js/classes/createRecipe.class.js
@@ -4,6 +4,7 @@ class CreateRecipe extends Base {
     this.app = app;
     this.load().then((data) => {
       this.ingredientsOptions = data;
+      this.imgUpload();
     });
     this.eventHandlers();
     this._stepsList = [];
@@ -39,6 +40,14 @@ class CreateRecipe extends Base {
     this._time = val;
   }
 
+  get img() {
+    return this._img;
+  }
+
+  set img(val) {
+    this._img = val;
+  }
+
 
 
 
@@ -59,10 +68,28 @@ class CreateRecipe extends Base {
 
     }
 
+    if (target.hasClass("upload-file-btn")) {
+
+
+
+
+
+
+
+
+
+
+
+
+      this.fileSelectHandler(event);
+
+    }
+
     if (target.hasClass("submint-btn")) {
       this.calcPortionNutrition();
       let json = this.createRecipe();
-      this.saveRecipe(json);
+      // console.log(json)
+      //this.saveRecipe(json);
 
     }
 
@@ -85,6 +112,11 @@ class CreateRecipe extends Base {
     }
     //console.log(this._categoriesList);
 
+    //upload file
+    if ((target).hasClass("fileUpload")) {
+      //console.log("upload")
+      this.fileSelectHandler(event);
+    }
   }
 
   keyup(event) {
@@ -99,11 +131,135 @@ class CreateRecipe extends Base {
     if (target.hasClass("time")) {
       this._time = target.val();
     }
+  }
 
 
 
+  // img upload start here
+  imgUpload() {
+    let that = this;
+    that.uploadInit();
+  }
+
+  uploadInit() {
+    console.log("Upload Initialised");
+    let that = this;
+    let xhr = new XMLHttpRequest();
+    if (xhr.upload) {
+      //let fileDrag = $(".file-drag");
+      let fileDrag = document.getElementById('file-drag');
+      console.log(fileDrag)
+      fileDrag.addEventListener('dragover', (e) => {
+        that.fileDragHover(e);
+      })
+      fileDrag.addEventListener('dragleave', (e) => {
+        that.fileDragHover(e);
+      })
+      fileDrag.addEventListener('drop', (e) => {
+        that.fileSelectHandler(e);
+      })
+
+
+      // File Drop
+      //fileDrag.addEventListener('dragover', this.fileDragHover(this), false);
+      // fileDrag.addEventListener('dragleave', fileDragHover, false);
+      // fileDrag.addEventListener('drop', fileSelectHandler, false);
+    }
 
   }
+
+  fileDragHover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    $(e.target).addClass("modal-body file-upload");
+
+  }
+
+  fileSelectHandler(event) {
+    let that = this;
+    // Fetch FileList object
+    let files = event.target.files || event.dataTransfer.files;
+    console.log(files)
+    // Cancel event and hover styling
+    that.fileDragHover(event);
+
+    // // Process all File objects
+    for (let i = 0, f; f = files[i]; i++) {
+      that.parseFile(f);
+      that.uploadFile(f);
+    }
+  }
+
+
+  parseFile(file) {
+    let that = this;
+    console.log(file.name);
+    // var fileType = file.type;
+    // console.log(fileType);
+    that._img = encodeURI(file.name);
+
+    let isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(that._img);
+    if (isGood) {
+      //console.log("img")
+      $("#uploader-start").addClass("hidden");
+      $("#messages").append(
+        '<strong>' + that._img + '</strong>'
+      );
+      $("#file-image").removeClass("hidden");
+      $("#file-image").attr("src", '"./img/' + that._img + '"');
+      //$("#file-image").src=URL.createObjectURL(file);
+
+    } else {
+      $('#notimage').removeClass('hidden');
+      $('#file-upload-form').trigger("reset");
+    }
+  }
+
+  uploadFile(file) {
+    //console.log(file.size)
+    let xhr = new XMLHttpRequest();
+
+    // pBar = document.getElementById('file-progress'),
+    let fileSizeLimit = 1024; // In MB
+    if (xhr.upload) {
+
+
+      // Check if file is less than x MB
+      if (file.size <= fileSizeLimit * 1024 * 1024) {
+
+        xhr.open('POST', document.getElementById('file-upload-form').action, true);
+        xhr.setRequestHeader('X-File-Name', file.name);
+        xhr.setRequestHeader('X-File-Size', file.size);
+        xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+        xhr.send(file);
+      } else {
+        $("#messages").append(
+          '<strong> Please upload a smaller file (< ' + fileSizeLimit + ' MB). </strong>'
+        );
+
+      }
+    }
+  }
+
+
+
+
+
+  // fileDragHover(e, fileDrag) {
+  //   //var fileDrag = document.getElementById('file-drag');
+  //   //console.log("drag")
+  //   // e.stopPropagation();
+  //   // e.preventDefault();
+
+  //   //fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
+
+  //   //fileDrag.addClass('modal-body file-upload');
+  //   console.log(fileDrag)
+  // }
+
+  //img upload above
+
+
   createRecipe() {
     let newRecipe = {};
     newRecipe.favorite = true;
@@ -119,7 +275,7 @@ class CreateRecipe extends Base {
     newRecipe.instructions = this._stepsList;
     newRecipe.nutrition = this._nutrients;
     newRecipe.comments = [];
-   // newRecipe.⚙ = "Recipe";
+    // newRecipe.⚙ = "Recipe";
 
 
     return newRecipe;
@@ -239,16 +395,24 @@ class CreateRecipe extends Base {
   }
 
   saveRecipe(json) {
+    delete(json.ingredient);
+    console.log('json to save', json)
+    //return;
     let recipeList = [];
-    JSON._save('testCreate', {recept:json});
+    // JSON._save('testCreate', {
+    //   recept: json
+    // });
     // $.getJSON('/json/testCreate.json').then((data) => {
     //   recipeList = data;
     //   recipeList.push(json);
     //   console.log(recipeList)
-       JSON._save('testCreate', json).then(()=>{
-         console.log("open new window of this recipe")
-       })
-    //})
+    //json = {color:'yellow'};
+    JSON._save('testCreate', json).then((err) => {
+      err && (console.error(err));
+      $.getJSON('/json/testCreate.json').then((data) => {
+        console.log("open new window of this recipe", data)
+      });
+    })
   }
 
 
